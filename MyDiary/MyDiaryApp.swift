34144c1029999b4f3ff3d8c0ec29318d0aecdd94
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-07-10 13:53:32 +0700

    Initial Commit

---
+++ MyDiary/MyDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDiaryApp.swift
+//  MyDiary
+//
+//  Created by Hans Arthur Cupiterson on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MyDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
